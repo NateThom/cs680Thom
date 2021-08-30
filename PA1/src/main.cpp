a541949658c662395cebfbaa3c4635d3ed4ba08f
Nathan Thom <nathanthom@nevada.unr.edu>
2021-08-30 12:29:05 -0700

    CS680-20 Change the structure of main.cpp so that engine is declared only with the constructor which expects shader file paths

--- PA1/src/main.cpp
+++ PA1/src/main.cpp
@@ -8,41 +8,26 @@ int main(int argc, char **argv){
 
   // Collect command line arguments for fragment and vertex shader files
   std::string vertexShaderFilePath, fragmentShaderFilePath;
-  if(input.cmdOptionExists("-v") && input.cmdOptionExists("-f")) {
+
+  if(input.cmdOptionExists("-v")) {
     vertexShaderFilePath = input.getCmdOption("-v");
+  }
+  if(input.cmdOptionExists("-f")) {
     fragmentShaderFilePath = input.getCmdOption("-f");
+  }
 
-    // Start an engine and run it then cleanup after
-    Engine *engine = new Engine("PA1", 800, 600);
-    if(!engine->Initialize(vertexShaderFilePath, fragmentShaderFilePath))
-    {
-      printf("The engine failed to start.\n");
-      delete engine;
-      engine = NULL;
-      return 1;
-    }
-    engine->Run();
+  // Start an engine and run it then cleanup after
+  Engine *engine = new Engine("PA1", 800, 600);
+  if(!engine->Initialize(vertexShaderFilePath, fragmentShaderFilePath))
+  {
+    printf("The engine failed to start.\n");
     delete engine;
     engine = NULL;
+    return 1;
   }
-  else{
-    std::cout<<"***Command line arguments not detected, running with default vertex and fragment shaders***"<<std::endl;
-    std::cout<<"***To run program with custom shaders please specify shader file paths***"<<std::endl;
-    std::cout<<"***(i.e. './PA1 -v <path-to-vertex-shader> -f <path-to-fragment-shader>'***"<<std::endl;
-
-    // Start an engine and run it then cleanup after
-    Engine *engine = new Engine("PA1", 800, 600);
-    if(!engine->Initialize())
-    {
-      printf("The engine failed to start.\n");
-      delete engine;
-      engine = NULL;
-      return 1;
-    }
-    engine->Run();
-    delete engine;
-    engine = NULL;
-  }
+  engine->Run();
+  delete engine;
+  engine = NULL;
 
   return 0;
 }
